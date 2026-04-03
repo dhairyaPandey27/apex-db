b1bcf46d7c62a680d3eddfbb61413d4d6d21697e
dhairyaPandey27 <pandeydhairya27@gmail.com>
2026-04-03 16:29:32 +0530

    feat(test.py): add the test for the following - insertion,table-full,checking maximum and longest length of the string   and negative ID - update(db.c): adds changes according to the test.py

--- db.c
+++ db.c
@@ -1,3 +1,4 @@
+#include <complex.h>
 #include <stdbool.h>
 #include <stddef.h>
 #include <stdint.h>
@@ -13,8 +14,8 @@
 typedef struct {
 
 	uint32_t id;
-	char username[COLUMN_USERNAME_SIZE];
-	char email[COLUMN_EMAIL_SIZE];
+	char username[COLUMN_USERNAME_SIZE+1];
+	char email[COLUMN_EMAIL_SIZE+1];
 
 } Row;
 
@@ -65,7 +66,9 @@ typedef enum {
 typedef enum {
 
 	PREPARE_SUCCESS,
+	PREPARE_NEGATIVE_ID,
 	PREPARE_SYNTAX_ERROR,
+	PREPARE_STRING_TOO_LONG,
 	PREPARE_UNRECOGNIZED_STATEMENT
 
 } PrepareResult;
@@ -173,19 +176,42 @@ MetaCommandResult do_meta_command(InputBuffer* input_buffer) {
 
 }
 
+PrepareResult prepare_insert(InputBuffer* input_buffer, Statement* statement) {
+
+	statement->type=STATEMENT_INSERT;
+
+	char* keyword = strtok(input_buffer->buffer, " ");
+	char* id_string = strtok(NULL," ");
+	char* username = strtok(NULL," ");
+	char* email = strtok(NULL, " ");
+
+	if(id_string==NULL || username==NULL || email==NULL) {
+		return PREPARE_SYNTAX_ERROR;
+	}
+
+	int id = atoi(id_string);
+	if (id<0) {
+		return PREPARE_NEGATIVE_ID;
+	}
+	if(strlen(username)>COLUMN_USERNAME_SIZE) {
+		return PREPARE_STRING_TOO_LONG;
+	}
+	if(strlen(email)>COLUMN_EMAIL_SIZE) {
+		return PREPARE_STRING_TOO_LONG;
+	}
+
+	statement->row_to_insert.id=id;
+	strcpy(statement->row_to_insert.username,username);
+	strcpy(statement->row_to_insert.email,email);
+
+	return PREPARE_SUCCESS;
+
+}
+
 PrepareResult prepare_statement(InputBuffer* input_buffer, Statement* statement) {
 
 	if(strncmp(input_buffer->buffer, "insert",6)==0) {
-		statement->type=STATEMENT_INSERT;
-		
-		int args_assigned = sscanf(input_buffer->buffer, "insert %d %s %s", &(statement->row_to_insert.id),statement->row_to_insert.username,statement->row_to_insert.email);
-		if (args_assigned<3) {
-		
-			return PREPARE_SYNTAX_ERROR;
-
-		}
-
-		return PREPARE_SUCCESS;
+		return prepare_insert(input_buffer,statement);
 	}
 	if(strncmp(input_buffer->buffer,"select",6)==0) {
 		statement->type=STATEMENT_SELECT;
@@ -265,6 +291,15 @@ void free_table(Table* table) {
 
 int main(int argc, char* argv[]) {
 
+	int test_mode=0;
+	for(int i=0;i<argc;i++){
+		if(strcmp(argv[i],  "--test-mode")==0)
+		{
+			test_mode=1;
+			break;
+		}
+	}
+
 	Table* table = new_table();
 
 	//creates a buffer to hold whatever the user types
@@ -273,7 +308,9 @@ int main(int argc, char* argv[]) {
 	//an infinite loop
 	while(true) {
 
-		print_prompt();
+		if(!test_mode)
+			print_prompt();
+
 		read_input(input_buffer);
 
 		// if(strcmp(input_buffer->buffer,".exit")==0) {
@@ -297,6 +334,12 @@ int main(int argc, char* argv[]) {
 		switch (prepare_statement(input_buffer,&statement)) {
 			case (PREPARE_SUCCESS):
 				break;
+			case (PREPARE_NEGATIVE_ID):
+				printf("ID must be positive.\n");
+				continue;
+			case (PREPARE_STRING_TOO_LONG):
+				printf("String is too long.\n");
+				continue;
 			case (PREPARE_SYNTAX_ERROR):
 				printf("Syntax error. Could not parse statement.\n");
 				continue;
@@ -311,7 +354,7 @@ int main(int argc, char* argv[]) {
 				printf("Executed.\n");
 				break;
 			case(EXECUTE_TABLE_FULL):
-				printf("Error: Table full.\n");
+				printf("Table full\n");
 				break;
 		
 		}
---
+++ test.py
@@ -0,0 +1,45 @@
+import subprocess
+
+def run_script(commands):
+    result=subprocess.run(
+            ["./a.out","--test-mode"],
+            input = "\n".join(commands)+"\n",
+            capture_output=True,
+            text = True
+            )
+    return result.stdout.strip().split("\n")
+
+def test_inserts_and_retrieve():
+    output = run_script(["insert 1 user1 example.com","select",".exit"])
+    expected_output = ["Executed.","(1 user1 example.com)","Executed."]
+    assert (len(output)==len(expected_output))
+    for i,o in enumerate(expected_output):
+        assert o==expected_output[i]
+
+def test_table_full():
+    input = [f'insert {i} user{i} user{i}.com' for i in range(1500)]
+    output = run_script(input)
+    assert "Table full" in output 
+
+def test_insert_max_length_string():
+    username = "a"*32
+    email = "b"*255
+    input = [f'insert 1 {username} {email}',"select",".exit"]
+    expected_output = ["Executed.",f'(1, {username}, {email})',"Executed."]
+    output = run_script(input)
+    assert(len(output)==len(expected_output))
+    assert output==expected_output
+
+def test_long_length_string():
+    username = "a"*33
+    email = "b"*256
+    input = [f'insert 1 {username} {email}',"select",".exit"]
+    expected_output = ["String is too long.","Executed."]
+    output = run_script(input)
+    assert output==expected_output
+
+def test_ID_Negative():
+    input = ["insert -1 user1 user1.com","select",".exit"]
+    expected_output = ["ID must be positive.","Executed."]
+    output = run_script(input)
+    assert output==expected_output
